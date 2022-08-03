89a09c25423b6da3329e0e7be41512beefa35f33
Alexander Goponenko <agoponenko@gmail.com>
2022-08-03 11:39:19 -0400

    improve up_machine.sh

--- up_machine.sh
+++ up_machine.sh
@@ -4,9 +4,10 @@ do
 	then
 		echo "=== === === === creating $1"
 		vagrant up $1
+		echo "=== === === === restarting $1"
+		vagrant halt $1
 	fi
-	echo "=== === === === restarting $1"
-	vagrant reload $1
+	vagrant up $1
 	vagrant ssh -c "/xch/scripts/check_lustre.sh" $1
  	shift 
 done
