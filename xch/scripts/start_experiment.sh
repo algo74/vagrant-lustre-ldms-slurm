302677b3cbb06d6361c279ba61fc57a5564a06c6
Alexander Goponenko <agoponenko@gmail.com>
2022-08-07 13:43:17 -0400

    add start_experiment.sh for the 2022-08-07 step experiment

---
+++ xch/scripts/start_experiment.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+MYDIR=/xch/results
+OUT_FILE=$MYDIR/$(date +%F.%H%M%S)-experiment.log
+
+echo "Output file: ${OUT_FILE}"
+
+/xch/scripts/demonize.sh /xch/logs/experiment.pid "$OUT_FILE" /xch/base_exp/cio_scan.sh 6 false 512 2
