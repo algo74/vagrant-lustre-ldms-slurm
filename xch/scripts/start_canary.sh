0a84105f60c9b56b4d651e1874d792de87229522
Alexander Goponenko <agoponenko@gmail.com>
2022-08-10 20:33:25 -0400

    reverted start_canary.sh for another experiment (2022-08-10)

--- xch/scripts/start_canary.sh
+++ xch/scripts/start_canary.sh
@@ -4,4 +4,4 @@ OUT_FILE=$MYDIR/$(date +%F.%H%M%S)-canary.log
 
 echo "Output file: ${OUT_FILE}"
 
-/xch/scripts/demonize.sh /xch/logs/canary.pid "$OUT_FILE" /xch/canary/canary_script/canary_constant_probing.sh -a localhost:9999 -b 65536 -s 1 -f /lustre/all/tt -m 1
+/xch/scripts/demonize.sh /xch/logs/canary.pid "$OUT_FILE" /xch/canary/canary_script/canary_constant_probing.sh -a localhost:9999 -b 4096 -s 1 -f /lustre/all/tt -m 1
