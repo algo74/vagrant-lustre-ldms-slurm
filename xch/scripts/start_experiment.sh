57735d2faf70acedcf746c1d4d624af3bade2443
Alexander Goponenko <agoponenko@gmail.com>
2022-09-09 15:01:45 -0400

    change back workload size for exp 2022-09-09

--- xch/scripts/start_experiment.sh
+++ xch/scripts/start_experiment.sh
@@ -4,4 +4,4 @@ OUT_FILE=$MYDIR/$(date +%F.%H%M%S)-experiment.log
 
 echo "Output file: ${OUT_FILE}"
 
-/xch/scripts/demonize.sh /xch/logs/experiment.pid "$OUT_FILE" /xch/base_exp/cio_scan.sh 20 false 512 2
+/xch/scripts/demonize.sh /xch/logs/experiment.pid "$OUT_FILE" /xch/base_exp/cio_scan.sh 6 false 512 2
