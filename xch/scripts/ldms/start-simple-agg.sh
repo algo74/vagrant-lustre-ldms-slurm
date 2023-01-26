c73b4029c73a37e940d5c6f353da30e8dbb24d2b
Alexander Goponenko <agoponenko@gmail.com>
2023-01-26 10:06:33 -0500

    less verbosity in ldms logs

--- xch/scripts/ldms/start-simple-agg.sh
+++ xch/scripts/ldms/start-simple-agg.sh
@@ -3,4 +3,5 @@
 SCRIPT_PATH=/xch/scripts/ldms
 
 source ${SCRIPT_PATH}/ldmsd-paths.sh
-ldmsd -m 1M -x sock:10002 -l ${LOG_PATH}/agg.log -v DEBUG -r $PID_PATH/simple_agg.pid -c ${CONF_PATH}/simple_agg.conf
+# ldmsd -m 1M -x sock:10002 -l ${LOG_PATH}/agg.log -v DEBUG -r $PID_PATH/simple_agg.pid -c ${CONF_PATH}/simple_agg.conf
+ldmsd -m 1M -x sock:10002 -l ${LOG_PATH}/agg.log -v ERROR -r $PID_PATH/simple_agg.pid -c ${CONF_PATH}/simple_agg.conf
--- xch/scripts/ldms/start-simple-sampler.sh
+++ xch/scripts/ldms/start-simple-sampler.sh
@@ -7,4 +7,5 @@ SCRIPT_PATH=/xch/scripts/ldms
 
 source ${SCRIPT_PATH}/ldmsd-paths.sh
 
-ldmsd -x sock:10001 -a none -l ${LOG_PATH}/$HOSTNAME-sampler.log -v DEBUG -r $PID_PATH/simple_sampler.pid -c ${CONF_PATH}/simple_sampler.conf
+# ldmsd -x sock:10001 -a none -l ${LOG_PATH}/$HOSTNAME-sampler.log -v DEBUG -r $PID_PATH/simple_sampler.pid -c ${CONF_PATH}/simple_sampler.conf
+ldmsd -x sock:10001 -a none -l ${LOG_PATH}/$HOSTNAME-sampler.log -v ERROR -r $PID_PATH/simple_sampler.pid -c ${CONF_PATH}/simple_sampler.conf
