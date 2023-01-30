4ba1eadbeeed503a29132f7e70136c4bbca5d3de
Alexander Goponenko <agoponenko@gmail.com>
2023-01-30 09:56:56 -0500

    io - zero experiment

--- xch/scripts/workloads/test-sixjobs.sh
+++ xch/scripts/workloads/test-sixjobs.sh
@@ -7,28 +7,32 @@ PRE=${1:-2}
 CYCLES=${2:-2}
 N_JOBS=${3:-2}
 
+CUR_JOB_ID=None
+FIRST_JOB_ID=None
+LAST_JOB_ID=None
+
 job1() {
-  sbatch --job-name=job1 -N 1 --time=50 /xch/scripts/jobs/uni-batch 512 3 100
+  CUR_JOB_ID=$(sbatch --parsable --job-name=job1 -N 1 --time=50 /xch/scripts/jobs/uni-batch 512 3 100)
 }
 
 job2() {
-  sbatch --job-name=job2 -N 1 --time=40 /xch/scripts/jobs/uni-batch 512 2 300
+  CUR_JOB_ID=$(sbatch --parsable --job-name=job2 -N 1 --time=40 /xch/scripts/jobs/uni-batch 512 2 300)
 }
 
 job3() {
-  sbatch --job-name=job3 -N 1 --time=20 /xch/scripts/jobs/uni-batch 512 1 200
+  CUR_JOB_ID=$(sbatch --parsable --job-name=job3 -N 1 --time=20 /xch/scripts/jobs/uni-batch 512 1 200)
 }
 
 job4() {
-  sbatch --job-name=job4 -N 1 --time=20 /xch/scripts/jobs/uni-batch 512 1 400
+  CUR_JOB_ID=$(sbatch --parsable --job-name=job4 -N 1 --time=20 /xch/scripts/jobs/uni-batch 512 1 400)
 }
 
 job5() {
-  sbatch --job-name=job5 -N 1 --time=30 /xch/scripts/jobs/uni-batch 512 1 800
+  CUR_JOB_ID=$(sbatch --parsable --job-name=job5 -N 1 --time=30 /xch/scripts/jobs/uni-batch 512 1 800)
 }
 
 job6() {
-  sbatch --job-name=job6 -N 1 --time=40 /xch/scripts/jobs/sleep-batch
+  CUR_JOB_ID=$(sbatch --parsable --job-name=job6 -N 1 --time=40 /xch/scripts/jobs/sleep-batch)
 }
 
 dispatch() {
@@ -46,6 +50,9 @@ while [ $counter -le $PRE ]
 do
   job1
   source $DIR/slurm-wait-till-done.sh
+  if [[ $FIRST_JOB_ID == "None" ]]; then
+    FIRST_JOB_ID=$CUR_JOB_ID
+  fi
   job2
   source $DIR/slurm-wait-till-done.sh
   job3
@@ -78,6 +85,8 @@ do
   ((counter++))
 done
 
+LAST_JOB_ID=$CUR_JOB_ID
+
 echo "waiting for completion"
 
 source $DIR/slurm-wait-till-done.sh
@@ -87,3 +96,5 @@ end=`date +%s`
 runtime=$((end-start))
 
 echo "jobs completed in $runtime"
+
+grep -n 'error' /xch/logs/jobs/slurm-{${FIRST_JOB_ID}..${LAST_JOB_ID}}.out
