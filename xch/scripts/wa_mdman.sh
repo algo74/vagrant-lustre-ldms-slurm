652c150c96cf2da27f472c2554b8a976f9e8d36b
Alexander Goponenko <agoponenko@gmail.com>
2024-04-29 10:24:44 -0400

    workaround option support

---
+++ xch/scripts/wa_mdman.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+TOP=/usr/local/ovis
+P_ADD=$TOP/lib/python3.6/site-packages
+[[ "$PYTHONPATH" != "$P_ADD:"* ]] && export PYTHONPATH=$P_ADD:$PYTHONPATH
+[[ "$LD_LIBRARY_PATH" != "$TOP/lib/:"* ]] && export LD_LIBRARY_PATH=$TOP/lib/:$LD_LIBRARY_PATH
+python3 /xch/py-sim-serv/pysimserv3.py --config_file=/xch/scripts/mdman/workaround_config.yaml "$@"
