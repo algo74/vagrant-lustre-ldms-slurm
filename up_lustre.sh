884f2e4b4c602768ab6a8c57c932fbe849acf99d
Alexander Goponenko <agoponenko@gmail.com>
2024-04-29 10:12:33 -0400

    various changes: late commit

--- up_lustre.sh
+++ up_lustre.sh
@@ -1,3 +1,5 @@
+set -e
+
 vagrant up mds01
 vagrant up oss01
 vagrant up oss02
--- xch/scripts/ldms/start-simple-agg.sh
+++ xch/scripts/ldms/start-simple-agg.sh
@@ -4,4 +4,4 @@ SCRIPT_PATH=/xch/scripts/ldms
 
 source ${SCRIPT_PATH}/ldmsd-paths.sh
 # ldmsd -m 1M -x sock:10002 -l ${LOG_PATH}/agg.log -v DEBUG -r $PID_PATH/simple_agg.pid -c ${CONF_PATH}/simple_agg.conf
-ldmsd -m 1M -x sock:10002 -l ${LOG_PATH}/agg.log -v ERROR -r $PID_PATH/simple_agg.pid -c ${CONF_PATH}/simple_agg.conf
+ldmsd -m 10M -x sock:10002 -l ${LOG_PATH}/simple_agg.log -v ERROR -r $PID_PATH/simple_agg.pid -c ${CONF_PATH}/simple_agg.conf
